f15ef9ad92d8934aea04de9b4d2fa41b7e7c3775
nagaraju4479 <yajjuvarapu.nagaraju@gmail.com>
2021-03-28 20:14:21 +0530

    Adding local files to git

---
+++ src/main/java/com/selenium/practise/HomePage.java
@@ -0,0 +1,9 @@
+package com.selenium.practise;
+
+public class HomePage {
+	
+	public void homePage() {
+		System.out.println("Home Page");
+	}
+
+}
---
+++ src/main/java/com/selenium/practise/LoginPage.java
@@ -0,0 +1,9 @@
+package com.selenium.practise;
+
+public class LoginPage {
+	
+	public void loginPage() {
+		System.out.println("Login Page");
+	}
+
+}
